550fa0f480ff4b5bfe0bc5d85752f04a2b7710d2
Deyan Angelov <deyancho55@gmail.com>
2026-09-21 10:15:43 +0300

    changes

---
+++ Week 2/A2_T2.py
@@ -0,0 +1,9 @@
+print("Program starting")
+
+brand = input("Insert car brand: ")
+model = input("Insert car model: ")
+
+print(f'Car brand is "' ,brand,'"', sep="", end="")
+print(f" and the model is '{model}'.")
+
+print("Program ending")
--- Week 2/A2_T7.py
+++
@@ -1,9 +0,0 @@
-print("Program starting")
-
-brand = input("Insert car brand: ")
-model = input("Insert car model: ")
-
-print(f'Car brand is "' ,brand,'"', sep="", end="")
-print(f" and the model is '{model}'.")
-
-print("Program ending")
